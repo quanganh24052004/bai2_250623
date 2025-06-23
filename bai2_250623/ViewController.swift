1ffcd81e7080fa4f93926cff5adfbdd154405715
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-06-24 06:35:25 +0700

    Initial Commit

---
+++ bai2_250623/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bai2_250623
+//
+//  Created by Nguyễn Quang Anh on 24/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
